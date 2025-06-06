210686f8ad008b72a3e7447debdc548a9c6f9ebe
DavidK004 <davidkatrinka1995@gmaill.com>
2025-06-06 17:03:37 +0200

    pushed

---
+++ racunarskeMreze/teorija/30maj.md
@@ -0,0 +1,39 @@
+## gigabit ethernet
+
+u fizičkom sloju se razlikuju
+
+CSMA/CD
+
+slajd 30 skip
+
+## ADSL
+
+# Bežični LAN
+
+OFDM
+
+HR-DSSS (high rate dsss)
+
+DCF
+
+PCF
+
+CSMA/CA
+
+802.11
+
+u llc piše protokol (ko je iznad mene)
+
+servisi
+
+slajd 32 ne treba (prezentacija 3 valjda)
+
+
+
+## ALOHA
+
+
+od slajd 64 treba (ne znam za to pre slajda 64)
+
+
+# virtuelni LAN
---
+++ vebProgramiranje/projekat/index.js
@@ -0,0 +1,57 @@
+new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    
+
+    slidesPerView: 1,
+    spaceBetween: 0,
+
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+            allowTouchMove: false,
+        }
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
+
+new Swiper('.categories-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    
+    navigation: {
+        nextEl: '.categories .swiper-button-next',
+        prevEl: '.categories .swiper-button-prev',
+    },
+    slidesPerView: 2, 
+    spaceBetween: 0,
+    breakpoints: {
+        480: {
+            slidesPerView: 3, 
+            spaceBetween: 0,
+        },
+        640: {
+            slidesPerView: 4, 
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 6, 
+            spaceBetween: 0,
+        },
+        1024: {
+            slidesPerView: 8, 
+            spaceBetween: 0,
+        }
+    }
+});
